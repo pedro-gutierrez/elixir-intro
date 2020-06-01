d8f89e659677af70e7886971c42fc8dad1eedc1e
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-06-02 00:46:15 +0200

    optimized consumer

---
+++ consumer2.erl
@@ -0,0 +1,22 @@
+% optimized consumer.erl
+-module(consumer).
+-export([start/0]).
+-export([loop/1]).   % need this
+
+start() ->
+    spawn(fun() -> loop([]) end).
+
+loop(Items) when is_list(Items) -> % backwards compatibility
+    loop(length(Items));
+
+loop(Total) -> % optimized code
+    receive
+        switch ->
+            ?MODULE:loop(Total);
+    	  		
+        {From, total} ->
+            From ! {total, Total, v2},
+            loop(Total);
+        _ ->
+            loop(Total+1)
+    end.
